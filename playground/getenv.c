64d9791cb507d0027f1a681f38cc9fcd3ee533a2
kahori <9.moonlight.ginga@gmail.com>
2024-07-19 17:03:26 +0900

    fix exec

--- playground/getenv.c
+++ playground/getenv.c
@@ -5,40 +5,43 @@
 // ファイルが見つかればそのファイルのフルパスを返す
 // 見つからなければNULLを返す
 
-
- char *serch_path(const char *filename)
+char	*serch_path(const char *filename)
 {
-        char *path = getenv("PATH");
-        char *path_copy = strdup(path);
-        //strtok:区切り文字で区切ってトークンに分解s 
-        char *path_token = strtok(path_copy, ":");
-        char *full_path = malloc(strlen(path_token) + strlen(filename) + 2); // +2 for '/' and '\0'
-        if(!full_path)
-        {
-            free(path_copy);
-            return NULL;
-        }
-        while(path_token)
-        {
-            strcpy(full_path, path_token);
-            strcat(full_path, "/");
-            strcat(full_path, filename);
-            if(access(full_path, F_OK) == 0) //F_OK : file exists
-            {
-                free(path_copy);
-                return full_path;
-            }
-            path_token = strtok(NULL, ":");
-        }
-        free(path_copy);
-        return NULL;
-    
+	char	*path;
+	char	*path_copy;
+
+	path = getenv("PATH");
+	path_copy = strdup(path);
+    char *path_token = strtok(path_copy, ":");
+	char *full_path = malloc(strlen(path_token) + strlen(filename) + 2); //+2 for '/' and '\0'
+	
+    if (!full_path)
+	{
+		free(path_copy);
+		return (NULL);
+	}
+	while (path_token)
+	{
+		strcpy(full_path, path_token);
+		strcat(full_path, "/");
+		strcat(full_path, filename);
+		if (access(full_path, F_OK) == 0) //F_OK : file exists
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		path_token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
 }
 
-int main()
+int	main(void)
 {
-    char *path = serch_path("pwd");
-    printf("%s\n", path);
-    free(path);
-    return 0;
+	char	*path;
+
+	path = serch_path("pwd");
+	printf("%s\n", path);
+	free(path);
+	return (0);
 }
