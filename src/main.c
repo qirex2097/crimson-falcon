ffd984725befad6b59e06931e6eedf703d532e62
kahori <kahori@student.42tokyo.jp>
2024-07-20 21:19:50 +0900

    chore: Refactor search_path function to exec.c

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@ void	free_tok(t_token *tok)
 	free(tok);
 }
 
+
 void	free_argv(char **argv)
 {
 	int	i;
@@ -28,6 +29,38 @@ void	free_argv(char **argv)
 	free(argv);
 }
 /*--------------*/
+
+char	*search_path(const char *filename)
+{
+	char	*path;
+	char	*path_copy;
+
+	path = getenv("PATH");
+	path_copy = strdup(path);
+    char *path_token = strtok(path_copy, ":");
+	char *full_path = malloc(strlen(path_token) + strlen(filename) + 2); //+2 for '/' and '\0'
+
+    if (!full_path)
+	{
+		free(path_copy);
+		return (NULL);
+	}
+	while (path_token)
+	{
+		strcpy(full_path, path_token);
+		strcat(full_path, "/");
+		strcat(full_path, filename);
+		if (access(full_path, X_OK) == 0) 
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		path_token = strtok(NULL, ":");
+	}
+	free(path_copy);
+	return (NULL);
+}
+
 
 int interpret(char *line)
 {
@@ -50,22 +83,26 @@ int	main()
 {
 	char	*line;
 	int	status = 0;
+	char *full_path;
 
 	rl_outstream = stderr;
 	while(1)
 	{
 		line = readline("minishell$ ");
-		
 		if(!line)		
-			break ;
-		if(*line)
+			break;
+		//line:full path
+		if(line[0] != '/')
 		{
-			//add_history(line);
-			
+			full_path = search_path(line);
+			interpret(full_path);
+			free(full_path);
 		}
-		interpret(line);
+		else
+		{
+			interpret(line);
+		}
 		free(line);
 	}
 	exit(status);
 }
-
