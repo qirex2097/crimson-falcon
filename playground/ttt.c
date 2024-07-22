2fd484bf299c49ab355ae964dc9f1e2f8eb2dbaa
kahori <kahori@student.42tokyo.jp>
2024-07-22 21:07:49 +0900

    word split buff[100}

--- playground/ttt.c
+++ playground/ttt.c
@@ -9,32 +9,50 @@ char    *skip_blank(char *line)
     return current;
 }
 
-char    *word(char *line)
+char    *skip_word(char *line)
 {
-    char    *current = line;
-    while (*current != ' ' && *current)
-        current++;
-    return current;
+    char    *p = line;
 
-
+    while(*p != ' ' && *p)
+        p++;
+    return  p;
 }
 
 void tokenizer(char *line)
 {
+    char *p;
+    char *buff;
+    char *buffs[1000];
+    int i;
+
+    i = 0;
     while(*line)
     {
         line = skip_blank(line);
-        printf("%s\n", line);
-        break ;
+        if(*line == '\0')
+            break;
+        p = skip_word(line);
+        buff = malloc(sizeof(char)* (p - line + 1));
+        strncpy(buff, line, p-line);
+//        printf("buff:%s\n", buff);
+        line = p;
+        
+        buffs[i] = buff;
+        i++;
     }
-
+    buffs[i] = NULL;
+    i = 0;
+    while (buffs[i]) {
+        printf("%s\n", buffs[i]);
+        i++;
+    }
 }
 
 
 
 int main()
 {
-    char *line = "    ls -l  ";
+    char *line = "    ls    -l | cat -e";
     // printf("$%s$\n", skip_blank(line));
     tokenizer(line);
 }
