a19b6873fcb97a8bb3c77f37b9936bd2a8229320
kahori <9.moonlight.ginga@gmail.com>
2024-07-30 14:20:32 +0900

    no fix

--- test.sh
+++ test.sh
@@ -70,6 +70,7 @@ assert "echo '\"hello   world\"' '42Tokyo'"
 
 ## combination
 assert "echo hello'      world'"
-assert "echo hello '      world'"
+assert "echo hello\nworld"
+assert "echo -e "hello\nworld""
 
 cleanup
