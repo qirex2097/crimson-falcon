b8b338eb8ee40cd09974e8723090025a428bf252
kahori <kahori@student.42tokyo.jp>
2024-08-05 20:11:43 +0900

    refactor parser.c to improve expression parsing

--- playground/parser.c
+++ playground/parser.c
@@ -19,59 +19,6 @@ typedef struct s_node {
   int val;       // kindがND_NUMの場合のみ使う
 } t_node;
 
-typedef enum {
-    TK_NUM,
-    TK_RESERVED,
-    TK_EOF,
-} TokenKind;
-
-typedef struct s_token
-{
-    TokenKind kind;
-    struct s_token *next;
-    int val; // value
-    char    *str;
-}   t_token;
-
-/*-------------------------------------------*/
-
-// グローバル変数
-t_token *token;
-
-/*-------------------------------------------*/
-
-int expect_number()
-{
-    if(token->kind != TK_NUM)
-        exit(1);
-    int val = token->val;
-    token = token->next;
-    return (val);
-}
-
-bool consume(char op)
-{
-    if(token->kind != TK_RESERVED || token->str[0] != op)
-        return false;
-    token = token->next;
-    return true;
-}
-
-void expect(char op)
-{
-    if(token->kind != TK_RESERVED || token->str[0] != op)
-    {
-        fprintf(stderr, "Expected '%c'\n", op);
-        exit(1);
-    }
-    token = token->next;
-}
-
-bool at_eof()
-{
-    return token->kind == TK_EOF;
-}
-
 /*-------------------------------------------*/
 
 // 新しいノードを作る関数
@@ -105,99 +52,68 @@ t_node *new_node_num(int val)
 
 /*-------------------------------------------*/
 
-t_node *expr();
+t_node *expr(char   *token[]);
 t_node *mul();
-t_node *primary();
+
+bool is_numeric(const char *str) {
+    // 文字列が NULL もしくは空の場合、数字ではないと見なします
+    if (str == NULL || *str == '\0') {
+        return false;
+    }
+    // 文字列の各文字を調べます
+    while (*str) {
+        if (!isdigit(*str)) {
+            return false;  // 数字ではない文字が含まれている場合
+        }
+        str++;
+    }
+    return true;  // すべての文字が数字だった場合
+}
 
 //+ - の判定
-t_node *expr()
+t_node *expr(char   *token[])
 {
-    t_node *node = mul();
-    while(1)
+    t_node  *node;
+    int i = 0;
+
+    if(!is_numeric(token[i]))
     {
-        if(consume('+'))
-            node = new_node(ND_ADD, node, mul());
-        else if(consume('-'))
-            node = new_node(ND_SUB, node, mul());
-        else
-            return node;
+        perror("error!\n");
+        exit(1);
     }
-}
-
-//* / の判定
-t_node *mul()
-{
-    t_node *node = primary();
-    while(1)
+    else
     {
-        if(consume('*'))
-            node = new_node(ND_MUL, node, primary());
-        else if(consume('/'))
-            node = new_node(ND_DIV, node, primary());
-        else
-            return node;
-    }
-}
-
-// () numの判定
-t_node *primary()
-{
-    if(consume('('))
-    {
-        t_node *node = expr();
-        expect(')');
-        return node;
-    }
-    return new_node_num(expect_number());
-}
-
-int evaluate(t_node *node) {
-    if (node->kind == ND_NUM) {
-        return node->val;
-    }
-
-    int lhs_val = evaluate(node->lhs);
-    int rhs_val = evaluate(node->rhs);
-
-    switch (node->kind) {
-        case ND_ADD:
-            return lhs_val + rhs_val;
-        case ND_SUB:
-            return lhs_val - rhs_val;
-        case ND_MUL:
-            return lhs_val * rhs_val;
-        case ND_DIV:
-            if (rhs_val == 0) {
-                fprintf(stderr, "Division by zero error\n");
+        node = new_node_num(atoi(token[i]));
+        i++;
+        while(token[i])
+        {
+            if(strcmp(token[i], "+") == 0 && is_numeric(token[i+1]))
+            {
+                node = new_node(ND_ADD, node, new_node_num(atoi(token[i+1])));
+                i = i + 2;
+            }
+            else if(strcmp(token[i],"-") == 0 && is_numeric(token[i+1]))
+            {
+                node = new_node(ND_SUB, node, new_node_num(atoi(token[i+1])));
+                i = i + 2;
+                
+            }
+            else
+            {
+                perror("error!!\n");
                 exit(1);
             }
-            return lhs_val / rhs_val;
-        default:
-            fprintf(stderr, "Unknown node kind\n");
-            exit(1);
+        }
     }
+    return(node);
 }
 
-// main関数の実装
-int main() {
-    // 例として "(3 + 5) * 2" を解析して評価する
-    // トークン列を手動で設定
-    t_token tokens[] = {
-        {TK_NUM, &tokens[1], 3, "3"},
-        {TK_RESERVED, &tokens[2], 0, "+"},
-        {TK_NUM, &tokens[3], 9, "9"},
-        {TK_RESERVED, &tokens[4], 0, "*"},
-        {TK_NUM, &tokens[5], 2, "2"},
-        {TK_EOF, NULL, 0, ""}
-    };
-    token = tokens;
 
-    // 式を解析してASTを生成
-    t_node *ast = expr();
 
-    // ASTを評価
-    int result = evaluate(ast);
-    printf("Result: %d\n", result); // 出力: Result: 16
+int main()
+{
+    char    *token[] = {"100", "+" , "300", "-", "7", NULL};
 
-    return 0;
+    t_node  *node = expr(token);
+    return(0);
 }
