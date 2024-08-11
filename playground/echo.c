a0a9680602b10805647873224c245f9ecd02a560
kahori <9.moonlight.ginga@gmail.com>
2024-08-11 16:13:36 +0900

    option -n 対応

--- playground/echo.c
+++ playground/echo.c
@@ -6,32 +6,57 @@
 /*   By: kahori <kahori@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 13:23:25 by kahori            #+#    #+#             */
-/*   Updated: 2024/08/11 15:21:16 by kahori           ###   ########.fr       */
+/*   Updated: 2024/08/11 16:10:40 by kahori           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <libc.h>
+#include <stdbool.h>
+
+bool    check_n_option(const char *argv)
+{
+    size_t i;
+
+    if(strncmp(argv, "-n", 2) != 0)
+        return (false);
+    
+    i = 2;
+    while(argv[i] != '\0')
+    {
+        if(argv[i]!= 'n')
+            return (false);
+        i++;
+    }
+    return true;
+}
 
 int builtin_echo(char **argv)
 {
-    size_t i = 0;
+    size_t i = 1;
+    bool    print_newline = true;
+
+    while(argv[i] && check_n_option(argv[i]))
+    {
+        print_newline = false;
+        i++;
+    }
 
     while(argv[i])
     {
-        i = 1;
         write(STDOUT_FILENO, argv[i], strlen(argv[i]));
         if(argv[i + 1])
             write(STDOUT_FILENO, " ", 1);
         i++;
     }
-    write(1, "\n", 1);
+    if(print_newline == true)
+        write(1, "\n", 1);
     return(0);
 }
 
 int main(int ac, char **av)
 {
-    if(ac > 1)
-        return(builtin_echo(av));
-    return (0);
+    if(ac > 0)
+        builtin_echo(av);
+    return 0;
 }
