171208d33b9a26c7ae08a3015d0dc7e875af4b0d
Masaru Hori <qirex_rr@yahoo.co.jp>
2024-08-17 08:07:26 +0900

    exec.cの整理

--- src/exec.c
+++ src/exec.c
@@ -41,48 +41,77 @@ char	*search_path(const char *filename)
 	return (NULL);
 }
 
-void validate_access(const char *path, const char *filename)
+bool is_command_not_found(const char *path)
 {
-	if (path == NULL)
-		err_exit(filename, "command not found", 127);
-	if (access(path, F_OK) < 0)
-		err_exit(filename, "command not found", 127);
+	return (path == NULL || access(path, F_OK | X_OK) < 0);
 }
+
+int _exec_builtin_command(t_cmd *cmd, int prev_fd, int *pfd)
+{
+	int status;
+	int original_stdin = -1;
+	int original_stdout = -1;
+
+	if (prev_fd != -1) {
+		original_stdin = dup(STDIN_FILENO);//標準入力をとっておく
+		dup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+	}
+	if (cmd->next) {
+		original_stdout = dup(STDOUT_FILENO);//標準出力をとっておく
+		dup2(pfd[1], STDOUT_FILENO);
+		close(pfd[1]);
+	}
+	if (open_redir_file(cmd->redirects) < 0) {
+		return(1);//リダイレクトのファイルがオープンできない時は終了
+	}
+	status = exec_builtin_command(cmd->args);
+	// 標準入力と標準出力を戻す
+	if (original_stdin != -1) {
+		dup2(original_stdin, STDIN_FILENO);
+	}
+	if (original_stdout != -1) {
+		dup2(original_stdout, STDOUT_FILENO);
+	}
+	return(status);
+}
+
+void exec_child_process(t_cmd *cmd, int prev_fd, int *pfd)
+{
+	char **argv = cmd->args;
+	char *path;
+
+	reset_signal();
+	if (prev_fd != -1) {	//パイプの読み出し側が設定されていたら
+		dup2(prev_fd, STDIN_FILENO);
+		close(prev_fd);
+	}
+	if (cmd->next) {	//パイプの書き込み側を設定する
+		close(pfd[0]);
+		dup2(pfd[1], STDOUT_FILENO);
+		close(pfd[1]);
+	}
+	if (open_redir_file(cmd->redirects) < 0) {
+		exit(1);//リダイレクトのファイルがオープンできない時は子プロセス終了
+	}
+	if(strchr(argv[0], '/') == NULL)
+		path = search_path(argv[0]);
+	else
+		path = strdup(argv[0]);
+	if (is_command_not_found(path))//コマンドがない時は子プロセス終了
+		err_exit(path, "command not found", 127);
+	execve(path, argv, 0);
+	fatal_error("execve");//ここには来ない。
+ }
 
 int exec_cmd(t_cmd *cmd, int prev_fd, int *pfd)
 {
-	char *path;
-	char **argv = cmd->args;
 	int wstatus;
     pid_t pid;
 
     if (is_builtin(cmd->args))
     {
-        int original_stdin = -1;
-        int original_stdout = -1;
-
-        if (prev_fd != -1) {
-            original_stdin = dup(STDIN_FILENO);
-            dup2(prev_fd, STDIN_FILENO);
-            close(prev_fd);
-        }
-        if (cmd->next) {
-            original_stdout = dup(STDOUT_FILENO);
-            dup2(pfd[1], STDOUT_FILENO);
-            close(pfd[1]);
-        }
-		if (open_redir_file(cmd->redirects) < 0) {
-			return(1);//リダイレクトのファイルがオープンできない時は子プロセス終了
-		}
-        wstatus = exec_builtin_command(cmd->args);
-        // 標準入力と標準出力を戻す
-        if (original_stdin != -1) {
-            dup2(original_stdin, STDIN_FILENO);
-        }
-        if (original_stdout != -1) {
-            dup2(original_stdout, STDOUT_FILENO);
-        }
-        return(wstatus);
+		return(_exec_builtin_command(cmd, prev_fd, pfd));
     } 
 
 	pid = fork();
@@ -90,26 +119,7 @@ int exec_cmd(t_cmd *cmd, int prev_fd, int *pfd)
 		fatal_error("fork");
     if(pid == 0)
     {
-		reset_signal();
-		if (prev_fd != -1) {	//パイプの読み出し側が設定されていたら
-			dup2(prev_fd, STDIN_FILENO);
-			close(prev_fd);
-		}
-		if (cmd->next) {	//パイプの書き込み側を設定する
-			close(pfd[0]);
-			dup2(pfd[1], STDOUT_FILENO);
-			close(pfd[1]);
-		}
-		if (open_redir_file(cmd->redirects) < 0) {
-			exit(1);//リダイレクトのファイルがオープンできない時は子プロセス終了
-		}
-		if(strchr(argv[0], '/') == NULL)
-			path = search_path(argv[0]);
-		else
-			path = strdup(argv[0]);
-		validate_access(path, argv[0]);//コマンドがない時は子プロセス終了
-		execve(path, argv, 0);
-		fatal_error("execve");//ここには来ない。
+		exec_child_process(cmd, prev_fd, pfd);
     }
     else
     {
@@ -119,7 +129,7 @@ int exec_cmd(t_cmd *cmd, int prev_fd, int *pfd)
 		if (cmd->next) {
 			close(pfd[1]);
 		}
-        wait(&wstatus);
+        wait(&wstatus);//子プロセスの終了待ち
 		return(wstatus);
     }
 	
