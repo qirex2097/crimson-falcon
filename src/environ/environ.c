f6b411022d3ca659d69fa55718d03cd3f7d19d51
Masaru Hori <qirex_rr@yahoo.co.jp>
2024-08-29 06:13:47 +0900

    parse_cmd_error の戻り値はエラーの時にトークン

--- src/environ/environ.c
+++ src/environ/environ.c
@@ -1,201 +1,217 @@
 #include "minishell.h"
 
-char *ft_strndup(const char *str, int n)
+char	*ft_strndup(const char *str, int n)
 {
-    char *line;
-    int i;
+	char	*line;
+	int		i;
 
-    line = malloc(sizeof(char) * (n + 1));
-    i = 0;
-    while (i < n)
-    {
-        line[i] = str[i];
-        i++;
-    }
-    line[i] = '\0';
-    return(line);
+	line = malloc(sizeof(char) * (n + 1));
+	i = 0;
+	while (i < n)
+	{
+		line[i] = str[i];
+		i++;
+	}
+	line[i] = '\0';
+	return (line);
 }
 
-int ft_isalpha(int c)
+int	ft_isalpha(int c)
 {
-    if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
-        return 1;
-    else
-        return 0;
+	if (('A' <= c && c <= 'Z') || ('a' <= c && c <= 'z'))
+		return (1);
+	else
+		return (0);
 }
 
-int ft_isalnum(int c)
+int	ft_isalnum(int c)
 {
-    if (ft_isalpha(c) || ('0' <= c && c <= '9'))
-        return 1;
-    else 
-        return 0;
-}
-//----------------------------------------
-t_env *new_env_var(const char *key, const char *value)
-{
-    t_env *new_var = malloc(sizeof(t_env));
-    new_var->key = ft_strdup(key);
-    new_var->value = ft_strdup(value);
-    new_var->next = NULL;
-    return(new_var);
+	if (ft_isalpha(c) || ('0' <= c && c <= '9'))
+		return (1);
+	else
+		return (0);
 }
 
-void free_env_var(t_env *env)
+t_env	*new_env_var(const char *key, const char *value)
 {
-    free(env->key);
-    free(env->value);
-    free(env);
-    return;
+	t_env	*new_var;
+
+	new_var = malloc(sizeof(t_env));
+	new_var->key = ft_strdup(key);
+	new_var->value = ft_strdup(value);
+	new_var->next = NULL;
+	return (new_var);
 }
 
-t_env *initialize_env()
+void	free_env_var(t_env *env)
 {
-    t_env head;
-    t_env *new_env;
-    extern char **environ;
-    int i;
-    char *equals;
-    char key[1024];
-    char value[1024];
-
-    i = 0;
-    head.next = NULL;
-    while (environ[i])
-    {
-        equals = ft_strchr(environ[i], '=');
-        if (equals)
-        {
-            ft_strncpy(key, environ[i], equals - environ[i]);
-            key[equals - environ[i]] = '\0';
-            ft_strncpy(value, equals + 1, ft_strlen(equals + 1) + 1);
-            new_env = new_env_var(key, value);
-            new_env->next = head.next;
-            head.next = new_env;
-        }
-        i++;
-    }
-    return(head.next);
+	free(env->key);
+	free(env->value);
+	free(env);
+	return ;
 }
 
-void cleanup_env(t_env *env_root)
+t_env	*initialize_env_loop(char *start, char *end)
 {
-    t_env *current;
+	char	key[1024];
+	char	value[1024];
 
-    current = env_root;
-    while (current)
-    {
-        t_env *next = current->next;
-        free_env_var(current);
-        current = next;
-    }
+	ft_strncpy(key, start, end - start);
+	key[end - start] = '\0';
+	ft_strncpy(value, end + 1, ft_strlen(end + 1) + 1);
+	return (new_env_var(key, value));
 }
 
-char **create_env_array(t_env *env_root)
+t_env	*initialize_env(void)
 {
-    int count, i;
-    t_env *current;
-    char **env_array;
+	t_env		head;
+	t_env		*new_env;
+	extern char	**environ;
+	int			i;
+	char		*equals;
 
-    count = 0;
-    current = env_root;
-    while(current)
-    {
-        count++;
-        current = current->next;
-    }
-    
-    env_array = malloc(sizeof(char *) * (count + 1));
-    if (env_array == NULL)
-        fatal_error("malloc");
-    i = 0;
-    current = env_root;
-    while (current)
-    {
-        env_array[i] = malloc(ft_strlen(current->key) + ft_strlen(current->value) + 2);
-        sprintf(env_array[i], "%s=%s", current->key, current->value);
-        i++;
-        current = current->next;
-    }
-    env_array[i] = NULL;
-    
-    return env_array;
+	i = 0;
+	head.next = NULL;
+	while (environ[i])
+	{
+		equals = ft_strchr(environ[i], '=');
+		if (equals)
+		{
+			new_env = initialize_env_loop(environ[i], equals);
+			new_env->next = head.next;
+			head.next = new_env;
+		}
+		i++;
+	}
+	return (head.next);
 }
 
-bool is_valid_env_name(const char *key)
+void	cleanup_env(t_env *env_root)
 {
-    if (!key || !(*key) || (!ft_isalpha(*key) && *key != '_'))
-        return false;
-    while (*key)
-    {
-        if (!ft_isalnum(*key) && *key != '_')
-            return false;
-        key++;
-    }
-    return true;
+	t_env	*current;
+	t_env	*next;
+
+	current = env_root;
+	while (current)
+	{
+		next = current->next;
+		free_env_var(current);
+		current = next;
+	}
 }
 
-const char *ms_getenv(const char *key)
+char	**malloc_env_array(t_env *env_root)
 {
-    t_env *env;
-    
-    env = g_env_root.next;
-    while(env)
-    {
-        if (ft_strcmp(key, env->key) == 0)
-            return(env->value);
-        env = env->next;
-    }
-    return(NULL);
+	t_env	*current;
+	int		count;
+
+	count = 0;
+	current = env_root;
+	while (current)
+	{
+		count++;
+		current = current->next;
+	}
+	return (malloc(sizeof(char *) * (count + 1)));
 }
 
-int ms_setenv(const char *key, const char *value, int overwrite)
+char	**create_env_array(t_env *env_root)
 {
-    t_env *env;
-    t_env *new_env;
+	t_env	*current;
+	char	**env_array;
+	int		i;
 
-    env = g_env_root.next;
-    while(env)
-    {
-        if (ft_strcmp(env->key, key) == 0)
-        {
-            if (overwrite != 0)
-            {
-                free(env->value);
-                env->value = ft_strdup(value);
-            }
-            return(0);
-        }
-        env = env->next;
-    }
-    new_env = new_env_var(key, value);
-    if (new_env == NULL)
-    {
-        fatal_error("malloc");
-        return(-1);
-    }
-    new_env->next = g_env_root.next;
-    g_env_root.next = new_env;
-    return(0);
+	env_array = malloc_env_array(env_root);
+	if (env_array == NULL)
+		fatal_error("malloc");
+	i = 0;
+	current = env_root;
+	while (current)
+	{
+		env_array[i] = malloc(ft_strlen(current->key)
+				+ ft_strlen(current->value) + 2);
+		sprintf(env_array[i], "%s=%s", current->key, current->value);
+		i++;
+		current = current->next;
+	}
+	env_array[i] = NULL;
+	return (env_array);
 }
 
-int ms_unsetenv(const char *key)
+bool	is_valid_env_name(const char *key)
 {
-    t_env *prev_env;
-    t_env *env;
+	if (!key || !(*key) || (!ft_isalpha(*key) && *key != '_'))
+		return (false);
+	while (*key)
+	{
+		if (!ft_isalnum(*key) && *key != '_')
+			return (false);
+		key++;
+	}
+	return (true);
+}
 
-    prev_env = &g_env_root;
-    env = g_env_root.next;
-    while(env)
-    {
-        if (ft_strcmp(env->key, key) == 0)
-        {
-            prev_env->next = env->next;
-            free(env);
-            return(0);
-        }
-        prev_env = env;
-        env = env->next;
-    }
-    return(0);
+const char	*ms_getenv(const char *key)
+{
+	t_env	*env;
+
+	env = g_env_root.next;
+	while (env)
+	{
+		if (ft_strcmp(key, env->key) == 0)
+			return (env->value);
+		env = env->next;
+	}
+	return (NULL);
+}
+
+int	ms_setenv(const char *key, const char *value, int overwrite)
+{
+	t_env	*env;
+	t_env	*new_env;
+
+	env = g_env_root.next;
+	while (env)
+	{
+		if (ft_strcmp(env->key, key) == 0)
+		{
+			if (overwrite != 0)
+			{
+				free(env->value);
+				env->value = ft_strdup(value);
+			}
+			return (0);
+		}
+		env = env->next;
+	}
+	new_env = new_env_var(key, value);
+	if (new_env == NULL)
+	{
+		fatal_error("malloc");
+		return (-1);
+	}
+	new_env->next = g_env_root.next;
+	g_env_root.next = new_env;
+	return (0);
+}
+
+int	ms_unsetenv(const char *key)
+{
+	t_env	*prev_env;
+	t_env	*env;
+
+	prev_env = &g_env_root;
+	env = g_env_root.next;
+	while (env)
+	{
+		if (ft_strcmp(env->key, key) == 0)
+		{
+			prev_env->next = env->next;
+			free(env);
+			return (0);
+		}
+		prev_env = env;
+		env = env->next;
+	}
+	return (0);
 }
